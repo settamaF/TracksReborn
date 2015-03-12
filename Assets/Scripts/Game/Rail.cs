4566b87b73e461a3b1e556929916642aaa7457e3
f.settama <f.settama@isartdigital.com>
2015-03-12 14:21:02 +0100

    Update to unity 5.0

---
+++ Assets/Scripts/Game/Rail.cs
@@ -0,0 +1,48 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//******************************************************************************
+public class Rail : MonoBehaviour 
+{
+#region Script Parameters
+	public List<Vector3> Points;
+#endregion
+
+#region Static
+#endregion
+
+#region Properties
+
+#endregion
+
+#region Fields
+	// Const -------------------------------------------------------------------
+
+	// Private -----------------------------------------------------------------
+#endregion
+
+#region Unity Methods
+
+#endregion
+
+#region Methods
+	public void AddRail(List<Vector3> newRail)
+	{
+
+	}
+
+	public void Remove(int countPoint)
+	{
+
+	}
+#endregion
+
+#region Implementation
+
+#endregion
+}
