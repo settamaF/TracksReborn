0ebd3da4421e51f3c5e2fbc83cdfb591a52cc6bf
f.settama <f.settama@isartdigital.com>
2015-04-05 18:51:27 +0100

    clean + pool

---
+++ Assets/Scripts/GeneratePattern/Object.cs
@@ -0,0 +1,216 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//******************************************************************************
+public class Object : MonoBehaviour 
+{
+	public class ObjectChild
+	{
+		public ObjectType		Type;
+		public int				ChildCount;
+		public List<Transform>	Childs;
+	}
+
+	[System.Serializable]
+	public class RateObject
+	{
+		public ObjectType		Type;
+		public int				Rate;
+	}
+
+#region Script Parameters
+	public ObjectType			Type;
+	public bool					Static;
+	public List<RateObject>		ListRate;
+#endregion
+
+#region Static
+#endregion
+
+#region Properties
+	private Transform mNext;
+	public Transform Next { get { return mNext; } }
+
+#endregion	
+
+#region Fields
+	// Const -------------------------------------------------------------------
+
+	// Private -----------------------------------------------------------------
+	private List<ObjectChild> mObjectChild;
+#endregion
+
+#region Unity Methods
+	void Awake()
+	{
+		if (Type == ObjectType.GROUND)
+		{
+			mNext = this.transform.FindChild("Next");
+			if (!mNext)
+			{
+				Debug.LogError("Error no child Next in " + this.name);
+				return;
+			}
+			Initialize();
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		foreach (Transform child in transform)
+		{
+			Gizmos.color = GetDebugColor(child.name.ToUpper());
+			if (Gizmos.color != Color.black)
+				Gizmos.DrawSphere(child.position, 0.5f);
+		}
+	}
+#endregion
+
+#region Methods
+	public void Initialize()
+	{
+		mObjectChild = new List<ObjectChild>();
+		for (int i = 0; i <= PoolGenerator.mMaxEnumType; i++)
+		{
+			ObjectChild objectChild = new ObjectChild();
+
+			objectChild.Type = (ObjectType)i;
+			objectChild.Childs = Initialize((ObjectType)i);
+			objectChild.ChildCount = objectChild.Childs.Count;
+			mObjectChild.Add(objectChild);
+		}
+	}
+
+	public void Use(ref bool eventActive, ref int eventDuration, ref bool setEventEnd)
+	{
+		foreach (var objectChild in mObjectChild)
+		{
+			foreach (Transform child in objectChild.Childs)
+			{
+				if (child.childCount > 0 || !RateGenerate(objectChild.Type))
+					continue;
+				if (objectChild.Type == ObjectType.EVENT && eventActive)
+				{
+					if (eventDuration <= 0)
+					{
+						eventActive = false;
+						setEventEnd = true;
+					}
+					else
+					{
+						eventDuration--;
+						continue;
+					}
+				}
+				GameObject gameObject;
+				if (objectChild.Type == ObjectType.EVENT && setEventEnd)
+				{
+					setEventEnd = false;
+					gameObject = PoolGenerator.Get.GetObject(ObjectType.EVENT, "End Event");
+					if (gameObject)
+					{
+						gameObject.GetComponent<EventTrigger>().Reset();
+						gameObject.transform.parent = child;
+						gameObject.transform.localPosition = Vector3.zero;
+					}
+				}
+				else
+				{
+					gameObject = PoolGenerator.Get.GetRandomObject(objectChild.Type);
+					if (gameObject)
+					{
+						gameObject.transform.parent = child;
+						gameObject.transform.localPosition = Vector3.zero;
+						if (objectChild.Type == ObjectType.EVENT)
+						{
+							eventActive = true;
+							gameObject.GetComponent<EventTrigger>().Reset();
+							eventDuration = gameObject.GetComponent<EventTrigger>().Duration;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void Reset()
+	{
+		foreach (var objectChild in mObjectChild)
+		{
+			foreach (Transform child in objectChild.Childs)
+			{
+				if (child.childCount > 0)
+				{
+					for (int i = 0; i < child.childCount; i++)
+					{
+						if (!child.GetChild(i).GetComponent<Object>().Static)
+							PoolGenerator.Get.AddToStack(child.GetChild(i).gameObject, objectChild.Type);
+					}
+				}
+			}
+		}
+		PoolGenerator.Get.AddToStack(this.gameObject, Type);
+	}
+#endregion
+
+#region Implementation
+
+	private List<Transform> Initialize(ObjectType type)
+	{
+		List<Transform> ret = new List<Transform>();
+
+		foreach (Transform child in this.transform)
+		{
+			if (child.name.ToUpper() == type.ToString())
+			{
+				ret.Add(child);
+			}
+		}
+		return ret;
+	}
+
+	private Color GetDebugColor(string type)
+	{
+		switch (type)
+		{
+			case "BLIND":
+				return Color.red;
+			case "EVENT":
+				return Color.green;
+			case "GROUND":
+				return Color.blue;
+			case "OBSTACLE":
+				return Color.white;
+			default:
+				break;
+		}
+		return Color.black;
+	}
+
+	private bool RateGenerate(ObjectType type)
+	{
+		int rand = Random.Range(0, 101);
+		int rate = 0;
+
+		if (ListRate.Count <= 0)
+			return true;
+		foreach (var rates in ListRate)
+		{
+			if (rates.Type == type)
+			{
+				rate = rates.Rate;
+				break;
+			}
+		}
+
+		if (rand >= rate)
+			return true;
+		return false;
+	}
+#endregion
+}
---
+++ Assets/Scripts/GeneratePattern/PoolGenerator.cs
@@ -0,0 +1,251 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA
+//******************************************************************************
+
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+//******************************************************************************
+
+public enum ObjectType
+{
+	OBSTACLE = 0,
+	BLIND,
+	GROUND,
+	EVENT
+}
+
+[System.Serializable]
+public class Stacks
+{
+	public ObjectType Type;
+	public List<ObjectStack> Stack;
+
+	public Stacks(ObjectType type, List<ObjectStack> stack)
+	{
+		Type = type;
+		Stack = stack;
+	}
+}
+
+[System.Serializable]
+public class ObjectStack
+{
+	public string				Name;
+	public Stack<GameObject>	Objects;
+}
+
+[System.Serializable]
+public class ObjectPrefab
+{
+	public ObjectType Type;
+	public List<GameObject> Prefabs;
+}
+
+public class PoolGenerator : MonoBehaviour 
+{
+#region Script Parameters
+	public List<ObjectPrefab> ObjectPrefabs;
+#endregion
+
+#region Static
+	public static int mMaxEnumType;
+#endregion
+
+#region Properties
+	private static PoolGenerator mInstance;
+	public static PoolGenerator Get {get {return mInstance;}}
+#endregion
+
+#region Fields
+	// Const -------------------------------------------------------------------
+	private const int	DEFAULT_DUPLICATION = 10;
+	private const int	OFFSETX = -1000;
+	private const int	OFFSETY = -1000;
+
+	// Private -----------------------------------------------------------------
+	private List<Stacks> mPool;
+#endregion
+
+#region Unity Methods
+	void Awake()
+	{
+		if (mInstance != null && mInstance != this)
+		{
+			Debug.LogWarning("PoolGenerator - we were instantiating a second copy of PoolGenerator, so destroying this instance");
+			DestroyImmediate(this.gameObject, true);
+			return;
+		}
+		mInstance = this;
+		DontDestroyOnLoad(this);
+		Initialization();
+	}
+#endregion
+
+#region Methods
+	public GameObject GetRandomObject(ObjectType type)
+	{
+		int range = 0;
+		int rand;
+
+		foreach(var stack in mPool)
+		{
+			if (stack.Type == type)
+			{
+				range = stack.Stack.Count;
+				if (range <= 0)
+					return null;
+				rand = Random.Range(0, range);
+				return GetObject(type, stack.Stack[rand].Name);
+			}
+		}
+		return null;
+	}
+
+	public GameObject GetObject(ObjectType type, string name)
+	{
+		int index = GetIndexStack(type);
+
+		if (index == -1)
+		{
+			Debug.LogWarning("Warning no object with " + type.ToString());
+			return null;
+		}
+		foreach (var stack in mPool[index].Stack)
+		{
+			if (stack.Name.ToLower() == name.ToLower())
+			{
+				GameObject ret;
+				if (stack.Objects.Count > 0)
+				{
+					ret = stack.Objects.Pop();
+					if (ret)
+						return ret;
+				}
+				ret = InstantiatePrefab(type, name);
+				return ret;
+			}
+		}
+		return null;
+	}
+
+	public void AddToStack(GameObject gameObject, ObjectType type)
+	{
+		foreach (var stacks in mPool)
+		{
+			if (stacks.Type == type)
+			{
+				foreach (var stack in stacks.Stack)
+				{
+					if (stack.Name == gameObject.name)
+					{
+						stack.Objects.Push(gameObject);
+						gameObject.transform.parent = this.transform;
+						gameObject.transform.localPosition = Vector3.zero;
+						break;
+					}
+				}
+			}
+		}
+	}
+#endregion
+
+#region Implementation
+
+	private void Initialization()
+	{
+		mMaxEnumType = (int)System.Enum.GetValues(typeof(ObjectType)).Cast<ObjectType>().Last();
+		this.transform.position = new Vector3(OFFSETX, OFFSETY, 0);
+		InitPoolGenerator();
+	}
+
+	private void InitPoolGenerator()
+	{
+		mPool = new List<Stacks>();
+		for (int i = 0; i <= mMaxEnumType; i++)
+		{
+			int index = GetIndexPrefab((ObjectType)i);
+			if (index >= 0)
+			{
+				List<ObjectStack> stack = new List<ObjectStack>();
+				foreach (var prefab in ObjectPrefabs[index].Prefabs)
+				{
+					ObjectStack obj = new ObjectStack();
+					obj.Name = prefab.name;
+					obj.Objects = GeneratePool(prefab);
+					stack.Add(obj);
+				}
+				mPool.Add(new Stacks((ObjectType)i, stack));
+			}
+		}
+	}
+
+	private int GetIndexPrefab(ObjectType type)
+	{
+		for (int i = 0; i < ObjectPrefabs.Count; i++)
+		{
+			if (ObjectPrefabs[i].Type == type)
+				return i;
+		}
+		return -1;
+	}
+
+	private int GetIndexStack(ObjectType type)
+	{
+		for (int i = 0; i < mPool.Count; i++)
+		{
+			if (mPool[i].Type == type)
+				return i;
+		}
+		return -1;
+	}
+
+	private Stack<GameObject> GeneratePool(GameObject prefab)
+	{
+		Stack<GameObject> ret = new Stack<GameObject>();
+
+		for (int i = 0; i < DEFAULT_DUPLICATION; i++)
+		{
+			GameObject obj = InstantiatePrefab(prefab);
+
+			if (obj)
+			{
+				ret.Push(obj);
+			}
+			else
+			{
+				Debug.LogError("Error instantiate prefab: " + prefab.name);
+				break;
+			}
+		}
+		return ret;
+	}
+
+	private GameObject InstantiatePrefab(ObjectType type, string name)
+	{
+		int index = GetIndexPrefab(type);
+
+		if (index < 0)
+			return null;
+		foreach(var prefab in ObjectPrefabs[index].Prefabs)
+		{
+			if (prefab.name == name)
+				return InstantiatePrefab(prefab);
+		}
+		return null;
+	}
+
+	private GameObject InstantiatePrefab(GameObject prefab)
+	{
+		GameObject ret;
+
+		ret = Instantiate(prefab) as GameObject;
+		ret.transform.parent = this.transform;
+		ret.transform.localPosition = Vector3.zero;
+		ret.name = prefab.name;
+		return ret;
+	}
+#endregion
+}
