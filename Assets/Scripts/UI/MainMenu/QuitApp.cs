25dd632826c33c70a8cc65264fdd88ccf01c9f1f
Blackhart <manceau.thomas19@gmail.com>
2015-04-21 11:38:04 +0200

    UI Base Added.

---
+++ Assets/Scripts/UI/MainMenu/QuitApp.cs
@@ -0,0 +1,52 @@
+﻿//******************************************************************************
+// Authors: Thomas MANCEAU
+//******************************************************************************
+
+using UnityEngine;
+using System.Collections;
+
+//******************************************************************************
+
+public class QuitApp : MonoBehaviour 
+{
+	#region Script Parameters
+
+	#endregion
+
+	#region Static
+
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Fields
+
+	#endregion
+
+	#region Unity Methods
+		void Awake()
+		{
+
+		}
+
+		void Start () 
+		{
+
+		}
+	#endregion
+
+	#region Methods
+
+	public void	Quit()
+	{
+		Application.Quit();
+	}
+
+	#endregion
+
+	#region Implementation
+
+	#endregion
+}
